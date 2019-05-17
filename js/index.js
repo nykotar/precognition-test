772b67281cae950b2e4ed3f7847f22ca26d29cd4
nykotar <popcornjuice@protonmail.com>
2019-05-16 22:58:17 -0300

    Implementing step system

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-//renderer process for index.html
+let stepIndex = 0
+let steps = document.querySelectorAll(".step")
+
+function nextStep() {
+    steps[stepIndex].classList.remove("visible")
+    stepIndex = stepIndex + 1
+    steps[stepIndex].classList.add("visible")
+}
+
+document.querySelector("#instructions-next").addEventListener('click', () => {
+    nextStep()
+})
